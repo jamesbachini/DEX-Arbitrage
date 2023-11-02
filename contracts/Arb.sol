cadfb2ffe90b42631165261ce68830366f158a81
James Bachini <james@mcambridge.com>
2023-11-02 14:35:59 +0000

    update to latest openzeppelin library

--- contracts/Arb.sol
+++ contracts/Arb.sol
@@ -17,17 +17,17 @@ interface IERC20 {
 }
 
 interface IUniswapV2Router {
-  function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts);
-  function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts);
+	function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts);
+	function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts);
 }
 
 interface IUniswapV2Pair {
-  function token0() external view returns (address);
-  function token1() external view returns (address);
-  function swap(uint256 amount0Out,	uint256 amount1Out,	address to,	bytes calldata data) external;
+	function token0() external view returns (address);
+	function token1() external view returns (address);
+	function swap(uint256 amount0Out,	uint256 amount1Out,	address to,	bytes calldata data) external;
 }
 
-contract Arb is Ownable {
+contract Arb is Ownable(msg.sender) {
 
 	function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount) private {
 		IERC20(_tokenIn).approve(router, _amount);
@@ -39,7 +39,7 @@ contract Arb is Ownable {
 		IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
 	}
 
-	 function getAmountOutMin(address router, address _tokenIn, address _tokenOut, uint256 _amount) public view returns (uint256) {
+		function getAmountOutMin(address router, address _tokenIn, address _tokenOut, uint256 _amount) public view returns (uint256) {
 		address[] memory path;
 		path = new address[](2);
 		path[0] = _tokenIn;
@@ -48,22 +48,22 @@ contract Arb is Ownable {
 		return amountOutMins[path.length -1];
 	}
 
-  function estimateDualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external view returns (uint256) {
+	function estimateDualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external view returns (uint256) {
 		uint256 amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
 		uint256 amtBack2 = getAmountOutMin(_router2, _token2, _token1, amtBack1);
 		return amtBack2;
 	}
-	
-  function dualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external onlyOwner {
-    uint startBalance = IERC20(_token1).balanceOf(address(this));
-    uint token2InitialBalance = IERC20(_token2).balanceOf(address(this));
-    swap(_router1,_token1, _token2,_amount);
-    uint token2Balance = IERC20(_token2).balanceOf(address(this));
-    uint tradeableAmount = token2Balance - token2InitialBalance;
-    swap(_router2,_token2, _token1,tradeableAmount);
-    uint endBalance = IERC20(_token1).balanceOf(address(this));
-    require(endBalance > startBalance, "Trade Reverted, No Profit Made");
-  }
+
+	function dualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external onlyOwner {
+		uint startBalance = IERC20(_token1).balanceOf(address(this));
+		uint token2InitialBalance = IERC20(_token2).balanceOf(address(this));
+		swap(_router1,_token1, _token2,_amount);
+		uint token2Balance = IERC20(_token2).balanceOf(address(this));
+		uint tradeableAmount = token2Balance - token2InitialBalance;
+		swap(_router2,_token2, _token1,tradeableAmount);
+		uint endBalance = IERC20(_token1).balanceOf(address(this));
+		require(endBalance > startBalance, "Trade Reverted, No Profit Made");
+	}
 
 	function estimateTriDexTrade(address _router1, address _router2, address _router3, address _token1, address _token2, address _token3, uint256 _amount) external view returns (uint256) {
 		uint amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
@@ -76,7 +76,7 @@ contract Arb is Ownable {
 		uint balance = IERC20(_tokenContractAddress).balanceOf(address(this));
 		return balance;
 	}
-	
+
 	function recoverEth() external onlyOwner {
 		payable(msg.sender).transfer(address(this).balance);
 	}
@@ -85,7 +85,7 @@ contract Arb is Ownable {
 		IERC20 token = IERC20(tokenAddress);
 		token.transfer(msg.sender, token.balanceOf(address(this)));
 	}
-	
+
 	receive() external payable {}
 
 }
